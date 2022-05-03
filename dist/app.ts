10f8313171279209565f5a09aee4ad58a379ecc3
Togashi-sys <t.togashi@strategit.jp>
2022-05-04 01:45:19 +0900

    :recycle: slacのAPIをクラス化

--- dist/app.ts
+++ dist/app.ts
@@ -1,89 +1,101 @@
 import 'google-apps-script'
+import './types'
+
+class SlackUser {
+  private user_token: string
+  private user_id: string
+  public url: string
+
+  user_profile: string[]
+
+  constructor(user: UserToken, url: string) {
+    this.user_token = user.token
+    this.user_id = user.id
+    this.url = url
+    this.user_profile = ['users', 'profile']
+  }
+
+  get Headers(): Object {
+    return {
+      Authorization: 'Bearer ' + this.user_token,
+      'X-Slack-User': this.user_id,
+      'Content-Type': 'application/json; charset=utf-8',
+    }
+  }
+
+  makeSlackAPIUrl(api_url: string[]) {
+    return [this.url, api_url.join('.')].join('')
+  }
+
+  makeOptions(method: string): Object {
+    return { method: method, headers: this.Headers }
+  }
+
+  get UserProfile() {
+    const api_point = this.user_profile.concat(['get'])
+    const options: Object = {
+      method: 'get',
+      headers: this.Headers,
+    }
+    const response = UrlFetchApp.fetch(this.makeSlackAPIUrl(api_point), options)
+    const resJson = JSON.parse(response.getContentText())
+    return resJson.profile
+  }
+
+  setUserProfile(status: UserStatus) {
+    const api_point = this.user_profile.concat(['set'])
+
+    const payload = {
+      profile: status,
+    }
+
+    const options: Object = {
+      method: 'post',
+      headers: this.Headers,
+      payload: JSON.stringify(payload),
+    }
+
+    UrlFetchApp.fetch(this.makeSlackAPIUrl(api_point), options)
+  }
+}
+
+function getSlackProps(): { user: UserToken; url: string } {
+  {
+    const google_properties =
+      PropertiesService.getScriptProperties().getProperties()
+
+    return {
+      user: {
+        token: google_properties['user_token'],
+        id: google_properties['user_id'],
+      },
+      url: google_properties['slack_url'],
+    }
+  }
+}
 
 function myFunction() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
   var sheet = spreadsheet.getActiveSheet()
   var range = sheet.getRange('A1:A115').getValues()
 
-  const value = range[Math.floor(Math.random() * range.length)]
+  const value: string[] = range[Math.floor(Math.random() * range.length)]
   console.log(value)
 
   const slack_props = getSlackProps()
 
-  if (hasSlackStatusText(slack_props)) return
-
-  changeSlackStatus(slack_props, value[0])
-}
-
-function getSlackProps() {
-  const google_properties =
-    PropertiesService.getScriptProperties().getProperties()
-
-  const slack_user_token = google_properties['user_token'],
-    slack_user_id = google_properties['user_id']
-
-  const headers: Headers = {
-    Authorization: 'Bearer ' + slack_user_token,
-    'X-Slack-User': slack_user_id,
-    'Content-Type': 'application/json; charset=utf-8',
-  }
-
-  const return_props = {
-    headers: headers,
-    getSlackUrl: getSlackUrl,
-  }
-
-  return return_props
-
-  function getSlackUrl(add_path: string) {
-    let slack_url = google_properties['slack_url']
-    return [slack_url, add_path].join('')
-  }
-}
-
-function hasSlackStatusText(props: SlackProps) {
-  const url = props.getSlackUrl('users.profile.get')
-
-  const options: Object = {
-    method: 'get',
-    headers: props.headers,
-  }
-  const response = UrlFetchApp.fetch(url, options)
-
-  const resJson = JSON.parse(response.getContentText())
-
-  const status_text: string = resJson.profile.status_text
-
-  return status_text ? true : false
-}
-
-function changeSlackStatus(props: SlackProps, emoji: string) {
-  const url = props.getSlackUrl('users.profile.set')
-
-  const payload = {
-    profile: {
-      status_emoji: emoji,
-      status_text: '',
+  const slack_api = new SlackUser(
+    {
+      token: slack_props.user.token,
+      id: slack_props.user.id,
     },
-  }
+    slack_props.url
+  )
 
-  const options: Object = {
-    method: 'post',
-    headers: props.headers,
-    payload: JSON.stringify(payload),
-  }
+  if (slack_api.UserProfile.status_text) return
 
-  const res = UrlFetchApp.fetch(url, options)
-  const resJson = JSON.parse(res.getContentText())
-}
-
-interface Headers {
-  Authorization: string
-  'X-Slack-User': string
-  'Content-Type': string
-}
-
-interface SlackProps {
-  headers: Headers
-  getSlackUrl: Function
+  slack_api.setUserProfile({
+    status_emoji: value[0],
+    status_text: '',
+  })
 }
